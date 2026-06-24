a1eb7b6ebd003e4d57d41d172deae8fbc63d6b44
Matt Linder <108966946+mlinder10@users.noreply.github.com>
2026-06-24 17:37:21 -0400

    changed result of admin endpoint

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -9,30 +9,48 @@ const postSchema = z.object({
 });
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const parsed = postSchema.safeParse(body);
-  if (!parsed.success) {
-    return new Response(JSON.stringify({ error: "Invalid request" }), {
-      status: 400,
-    });
+  try {
+    const body = await req.json();
+    const parsed = postSchema.safeParse(body);
+    if (!parsed.success) {
+      return new Response(
+        JSON.stringify({ success: false, error: "Invalid request" }),
+        {
+          status: 400,
+        },
+      );
+    }
+
+    const token = parsed.data.token;
+    const rows = await db
+      .select()
+      .from(adminToken)
+      .where(
+        and(eq(adminToken.token, token), eq(adminToken.is_consumed, false)),
+      );
+
+    if (!rows.length) {
+      return new Response(
+        JSON.stringify({ success: false, error: "Invalid token" }),
+        {
+          status: 401,
+        },
+      );
+    }
+
+    await db
+      .update(adminToken)
+      .set({ is_consumed: true })
+      .where(eq(adminToken.token, token));
+
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ success: false, error: "Internal server error" }),
+      {
+        status: 500,
+      },
+    );
   }
-
-  const token = parsed.data.token;
-  const rows = await db
-    .select()
-    .from(adminToken)
-    .where(and(eq(adminToken.token, token), eq(adminToken.is_consumed, false)));
-
-  if (!rows.length) {
-    return new Response(JSON.stringify({ error: "Invalid token" }), {
-      status: 401,
-    });
-  }
-
-  await db
-    .update(adminToken)
-    .set({ is_consumed: true })
-    .where(eq(adminToken.token, token));
-
-  return new Response(JSON.stringify({ success: true }), { status: 200 });
 }
